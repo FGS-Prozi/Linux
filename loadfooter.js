85d0b6e90563752ca84bd40a21306c031c4c977c
FGS-Prozi <162698691+FGS-Prozi@users.noreply.github.com>
2024-06-01 10:57:17 +0200

    Add files via upload

--- loadfooter.js
+++ loadfooter.js
@@ -1,10 +1,8 @@
-document.addEventListener('DOMContentLoaded', async () => {
+window.addEventListener('load', async () => {
     try {
-        // Określenie ścieżki do pliku stopki w zależności od lokalizacji
         const footerPath = window.location.pathname.endsWith('/index.html') || window.location.pathname === '/' ?
             './footer.html' : '../../footer.html';
 
-        // Ładowanie i wstawienie zawartości pliku stopki za pomocą fetch i async/await
         const response = await fetch(footerPath);
         if (!response.ok) throw new Error('Network response was not ok');
         document.getElementById('footer-placeholder').innerHTML = await response.text();
