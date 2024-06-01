0073b4076fd40ca3a4017a327929b35d9a493dfa
FGS-Prozi <162698691+FGS-Prozi@users.noreply.github.com>
2024-06-01 11:01:43 +0200

    Add files via upload

--- loadfooter.js
+++ loadfooter.js
@@ -1,12 +1,11 @@
-window.addEventListener('load', async () => {
-    try {
-        const footerPath = window.location.pathname.endsWith('/index.html') || window.location.pathname === '/' ?
-            './footer.html' : '../../footer.html';
+document.addEventListener('DOMContentLoaded', () => {
+    const footerPath = window.location.pathname === '/index.html' || window.location.pathname === '/' ?
+        './footer.html' : '../../footer.html';
 
-        const response = await fetch(footerPath);
-        if (!response.ok) throw new Error('Network response was not ok');
-        document.getElementById('footer-placeholder').innerHTML = await response.text();
-    } catch (error) {
-        console.error('Error loading footer:', error);
-    }
+    fetch(footerPath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
 });
